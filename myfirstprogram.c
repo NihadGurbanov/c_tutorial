0f701193ae4f404789f57da776e3081aafff6920
Nihad Gurbanov <nihad.gurbanov85@gmail.com>
2023-10-07 20:09:24 +0400

    version 1

---
+++ myfirstprogram.c
@@ -0,0 +1,29 @@
+//#include <stdio.h>
+//
+//int main() {
+//
+//    printf("Hello World!!!!!");
+//    char myNum;
+//    myNum = "A";
+//    printf("%c", myNum);
+//    return 0;
+//}
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main()
+//{
+//    char character;
+//    character = 'a';
+//    printf("character = %c\n",character);
+//    printf("character = %d,Stored as integer\n", character);
+//    return 0;
+//}
+
+#include <stdio.h>
+main()
+{
+    int num;
+    num = 2;
+    printf("%d", num);
+    }
